6fcdbc5a571cdd55ef120dad458e810c716c96d6
Ryan Miller <miller@redhat.com>
2022-09-30 14:54:35 -0400

    Add trailing message after ./run.sh

--- run.sh
+++ run.sh
@@ -25,3 +25,19 @@ oc exec -it -n vault-server vault-server-0 -- \
 
 echo "Creating objects under './manifests'..."
 oc create -f ./manifests
+
+cat << EOF
+
+-----
+
+If everything was successful, External Secrets should have created an OpenShift
+Secret named *message* in the *my-app* namespace. Validate the secret exists
+with:
+
+$ oc get secrets -n my-app message
+
+Print the contents of the secret with:
+
+$ oc get secrets -n my-app message -o jsonpath="{.data.message}" | base64 -d
+
+EOF
